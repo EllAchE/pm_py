c9356c3676e9a00c2d545f646c0cb613911b5506
EllAchE <lhar8771@gmail.com>
2021-08-31 23:57:47 -0500

    updates to take arguments positionally and not as keywords

--- local_api/local_api.py
+++ local_api/local_api.py
@@ -7,16 +7,34 @@ app = Flask(__name__)
 # 0x18f541b0844BEB2517A7B825a95402227536234b
 # Secretary firing by october test market address
 
-@app.route('/polybuy/<mmAddress>/<int:amount>/<int:outcomeIndex>/<float:minShares>/<int:gas>')
+# Test trigger url http://localhost:5000/polybuy/0x18f541b0844BEB2517A7B825a95402227536234b/0.5/1/0.52/15
+
+@app.route('/polybuy/<mmAddress>/<amount>/<outcomeIndex>/<minShares>/<gas>')
 def buyOrder(mmAddress, amount, outcomeIndex, minShares, gas):
-    print("inside help")
+    # Pring arguments to py console. Likely unseen
+    print("mmAddress", mmAddress)
+    print("amount", amount)
+    print("outcomeIndex", outcomeIndex)
+    print("minShares", minShares)
+    print("gas", gas)
+
+    # Actual purchase logic
     w3 = initialize_identity(gas)
-    print('initialized')
     try :
         trx_hash = buy(w3, mmAddress, amount, outcomeIndex, minShares)
     except Exception as err:
         trx_hash = err
     print(trx_hash)
-    print('hash made')
-    return trx_hash
+
+    print('hash made', trx_hash)
+
+    return {
+        "mmAddress": mmAddress,
+        "amount": amount,
+        "outcomeIndex": outcomeIndex,
+        "minShares": minShares,
+        "gas": gas,
+        "transactionHash": trx_hash
+    }
+
 app.run()
